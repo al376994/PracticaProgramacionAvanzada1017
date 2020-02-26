577990309343fb190d84077b3e9985c2714dbbac
al376994 <al376994@uji.es>
2020-02-26 09:14:05 +0100

    Añadida seguridad en el Input

--- src/main/java/Auxiliares/Input.java
+++ src/main/java/Auxiliares/Input.java
@@ -1,24 +1,49 @@
 package Auxiliares;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
 
 	private static Scanner s = new Scanner(System.in);
 
-	public String fromTerminalAnString(String text) {
+	public String fromTerminalAskString(String text) {
 		IO.out.toTerminal(text, " ");
 		return s.nextLine();
 	}
 
-	public int fromTerminalAnInt(String text) {
-		IO.out.toTerminal(text, " ");
-		return s.nextInt();
+	public int fromTerminalAskInt(String text) {
+		while (true) {
+			IO.out.toTerminal(text, " ");
+			try {
+				int input = s.nextInt();
+				s.nextLine();
+				return input;
+			} catch (InputMismatchException e) {
+				IO.out.toTerminal("Error, no se ha introducido un número.");
+				s.nextLine();
+			}
+		}
 	}
 
-	public double fromTerminalADouble(String text) {
-		IO.out.toTerminal(text, " ");
-		return s.nextDouble();
+	public double fromTerminalAskDouble(String text) {
+		while (true) {
+			IO.out.toTerminal(text, " ");
+			try {
+				double input = s.nextDouble();
+				s.nextLine();
+				return input;
+			} catch (InputMismatchException e) {
+				IO.out.toTerminal("Error, no se ha introducido un número.");
+				s.nextLine();
+			}
+		}
+	}
+
+	public void waitIntro() {
+		IO.out.toTerminal("Pulsa cualquier tecla para continuar.", " ");
+		s.nextLine();
+		s.nextLine();
 	}
 
 }
